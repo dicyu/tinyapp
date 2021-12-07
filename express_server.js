bc22861cac62412c4c34d805bb3f538a8ff9c62e
Dickson Yu <dickson.yu@email.com>
2021-12-07 18:52:41 -0500

    added saving of cookies, added login button/form

--- express_server.js
+++ express_server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser');
 const PORT = 8080; // => Default Port : 8080
 
 app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.ca'
 };
+
+let username = {};
 
 function generateRandomString() {
   const randomString = Math.random().toString(36).substring(2, 8);
@@ -15,13 +21,14 @@ function generateRandomString() {
   return randomString;  
 };
 
-
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-
-
 app.get('/', (req, res) => {
   res.send('Hello!');
+});
+
+// Login post, username cookies
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username)
+  res.redirect('/urls')
 });
 
 // Post new URL from form page, into database
@@ -64,14 +71,10 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 
 // Update a URL
 app.post('/urls/:shortURL', (req, res) => {
-
-  // grab the id and the new long url
   const shortURL = req.params.shortURL
   const longURL = req.body.newURL
   console.log(shortURL, longURL);
-  // update the database
   urlDatabase[shortURL] = longURL;
-  // render page
   res.redirect('/urls')
 });
 
